c5fc1d6a925cd5e88c7c5624607a4f5f8645a521
LuizFilho <luizfilho.dev@gmail.com>
2019-03-27 01:50:55 -0300

    Bug Redux-Persist no Reduxform

--- src/store/index.js
+++ src/store/index.js
@@ -5,7 +5,8 @@ import reducers from "./reducers";
 
 const persistConfig = {
   key: "root",
-  storage
+  storage,
+  blacklist: ["form"]
 };
 
 const persistedReducer = persistReducer(persistConfig, reducers);
